3d17a0f664206b8ecb6c0aa473992112eda40146
dnlbell <dnl.bell@gmail.com>
2018-10-18 06:11:34 -0700

    missed changes

--- components/Categories.js
+++ components/Categories.js
@@ -4,7 +4,6 @@ import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
 class Categories extends React.Component{
 
     constructor() {
-
         super()
         this.state = {
             catagory : [
