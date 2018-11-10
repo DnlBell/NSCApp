c3e3fbeff15ba82497b6515e049f16d032e73820
dnlbell <dnl.bell@gmail.com>
2018-11-10 12:32:52 -0800

    initial styling for search results and android back button added to router

--- components/Course/CourseRow.js
+++ components/Course/CourseRow.js
@@ -1,21 +1,37 @@
 import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, Image } from 'react-native';
+import { withRouter } from 'react-router-native';
 import PropTypes from 'prop-types';
 import Course from 'mspnmodel/distribution/course/course';
+import styles from '../../styles/CourseRow.js';
 
 //:TODO: This is just for starters.
 
 const CourseRow = (props) => {
     return (
         <View>
-            <Text>{props.course.title}</Text>
-            <Text>{props.course.price}</Text>
+            <View style = {{flex:1, flexDirection: 'row'}}>
+                <View>
+                    <Image source={{uri: props.course.images[0]}} style={styles.image}/>
+                </View>
+                <View style = {{flex:1}}>
+                    <TouchableOpacity onPress={() => props.history.push("/course", {id: props.course.id})} >
+                        <Text>{props.course.title}</Text>
+                    </TouchableOpacity>
+                    <Text>{props.course.rating}</Text>
+                    <Text>${props.course.price}</Text>
+                </View>
+            </View>
+            <TouchableOpacity onPress={() => props.history.push("/course", {id: props.course.id})} >
+                <Text>Details</Text>
+            </TouchableOpacity>
+            
         </View>
     );
 };
 
 CourseRow.propTypes = {
     course: PropTypes.objectOf(Course),
-}
+};
 
-export default CourseRow;
+export default withRouter(CourseRow);
---
+++ styles/CourseRow.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import { Colors, Margins, ScreenWidth} from'./Common';
+
+export default StyleSheet.create({
+
+    image:{
+        width: ScreenWidth/3 ,
+        height: ScreenWidth/3 ,
+        margin: Margins.HalfMargin
+    }
+
+});
