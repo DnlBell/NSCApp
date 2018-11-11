edecff6e7cb5a8cdb839b916b49abcc5307d6920
dnlbell <dnl.bell@gmail.com>
2018-11-10 20:06:50 -0800

    basic styling

--- components/Course/CourseRow.js
+++ components/Course/CourseRow.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Text, View, TouchableOpacity, Image } from 'react-native';
 import { withRouter } from 'react-router-native';
+import StarRating from 'react-native-star-rating';
 import PropTypes from 'prop-types';
 import Course from 'mspnmodel/distribution/course/course';
 import styles from '../../styles/CourseRow.js';
 
 //:TODO: This is just for starters.
+function insert(str, index, value) {
+    return str.substr(0, index) + value + str.substr(index);
+}
 
 const CourseRow = (props) => {
     return (
@@ -16,16 +20,24 @@ const CourseRow = (props) => {
                 </View>
                 <View style = {{flex:1}}>
                     <TouchableOpacity onPress={() => props.history.push("/course", {id: props.course.id})} >
-                        <Text>{props.course.title}</Text>
+                        <Text style={styles.title}>{props.course.title}</Text>
                     </TouchableOpacity>
-                    <Text>{props.course.rating}</Text>
-                    <Text>${props.course.price}</Text>
+                    <View style = {styles.starFrame}>
+                        <StarRating 
+                            disabled={true}
+                            starSize= {15}
+                            maxStars={5}
+                            rating={props.course.rating}
+                        />
+                    </View>
+                    <Text style={styles.price}>${insert(props.course.price.toString(), props.course.price.toString().length - 2,'.')}</Text>
                 </View>
             </View>
-            <TouchableOpacity onPress={() => props.history.push("/course", {id: props.course.id})} >
-                <Text>Details</Text>
+            <Text style={styles.description}>{props.course.description}</Text>
+            <TouchableOpacity style={styles.details} onPress={() => props.history.push("/course", {id: props.course.id})} >
+                <Text style={styles.detailsText}>View details</Text>
             </TouchableOpacity>
-            
+            <View style={styles.line}/>
         </View>
     );
 };
--- styles/CourseRow.js
+++ styles/CourseRow.js
@@ -1,12 +1,44 @@
 import { StyleSheet } from 'react-native';
-import { Colors, Margins, ScreenWidth} from'./Common';
+import { Colors, Margins, ScreenWidth, FontSizes} from'./Common';
 
 export default StyleSheet.create({
 
+    title:{
+        color:Colors.AccentColorLight,
+        fontSize:FontSizes.ReadingFontSize,
+        margin:Margins.HalfMargin
+    },
     image:{
         width: ScreenWidth/3 ,
         height: ScreenWidth/3 ,
         margin: Margins.HalfMargin
+    },
+    starFrame:{
+        width: ScreenWidth/4,
+        margin:Margins.HalfMargin
+    },
+    price:{
+        fontWeight:"bold",
+        fontSize:FontSizes.ReadingFontSize,
+        margin:Margins.HalfMargin
+    },
+    description:{
+        margin: Margins.HalfMargin,
+        fontSize:FontSizes.ReadingFontSize
+    },
+    details:{
+        backgroundColor: Colors.AccentColorLight,
+        padding: Margins.HalfMargin,
+        margin: Margins.HalfMargin,
+        width: ScreenWidth/4,
+        alignItems:'center'
+    },
+    detailsText:{
+        color:Colors.PrimaryColor
+    },
+    line:{
+        height:1,
+        backgroundColor:Colors.AccentColorDark
     }
 
 });
