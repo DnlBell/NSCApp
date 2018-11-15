719d0d9694370697f70bf975722d1b8e514476ba
ryanybrennan <ryanybrennan@msn.com>
2018-11-14 22:33:56 -0800

    able to get values to display with the hide/show aspect of the Course page. Temp fix for the CourseList indexing problem.

--- screens/Course.js
+++ screens/Course.js
@@ -1,21 +1,27 @@
-import React, {Component} from 'react';
-import { View, ScrollView, Text, Button, Image, TouchableHighlight } from 'react-native';
+import React, {PureComponent} from 'react';
+import { View, ScrollView, Text, Image, TouchableHighlight, FlatList } from 'react-native';
 import { withRouter } from 'react-router-native';
 import Footer from '../components/Footer';
+import urls from '../constants/urls';
+import catStyle from '../styles/Catagories';
 
-class Course extends Component{
-    state = {
-        course : {},
-        status: false
-};
+class Course extends PureComponent{
+    constructor(props){
+        super(props);
+        this.state = {
+            course : {},
+            status: false
+        };
+    }
+    
     toggleStatus(){
         this.setState({
           status:!this.state.status
         });
       }
 
-    componentWillMount(){
-        return fetch('https://mspnapi.dolehesten.org/mspnapi/course/'+this.props.location.state.id)
+    componentDidMount(){
+        return fetch(`${urls.mspnApiRoot}/course/${this.props.location.state.id}`)
         .then((response) => response.json())
         .then((responseJson) => {
             this.setState({
@@ -30,6 +36,32 @@ class Course extends Component{
         let dollars = this.state.course.price/100.00;
         return "$ " + parseFloat(Math.round(dollars * 100) / 100).toFixed(2);;
     }
+    getImages(){
+        if(this.state.course.images.length >= 1) {
+            this.state.course.images.map( (image) => {
+                <Image source={{uri: image}}/>
+            })
+        } else {
+            <Text>No Images</Text>
+        }
+        
+    }
+
+    getInfo() {
+        let obj = this.state.course.information; 
+        let keys = [];
+        for(let key in obj){
+            keys.push(obj[key]);   
+        }
+        _keyExtractor = (item, index) => item.label;
+        return (
+            <FlatList
+            data={keys}
+            renderItem={({item}) => <Text>{item.label}: {item.value}</Text>}
+            keyExtractor={_keyExtractor}
+            />
+        ) 
+    }
 
     render() {
         return (
@@ -38,17 +70,22 @@ class Course extends Component{
                     <View style={styles.contentContainer}>
                         <Text>{this.state.course.title}</Text>
                         <Text>Vendor: {this.state.course.vendorName}</Text>
-                        {/* <Image style={{width: 60, height: 60}} source={{uri: this.state.course.images[0]}} /> */}
-                        <Text>{JSON.stringify(this.state.course.images)}</Text>
+                        <Image source={{uri: `${this.state.course.images}`}} style={{width: 150, height: 150}}/>
                         <Text>Price: {this.setPrice()}</Text>
                         <Text>Rating: {this.state.course.rating}</Text>
                         <Text>Description: {this.state.course.description}</Text>
-                        <Button title="Home" onPress={() => this.props.history.push("/")}/>
+                        <Text>Audience: {this.state.course.audience} </Text>
+                        <Text>Seats Available: </Text>
+                        <Text>Class Size: </Text>
 
-                        <TouchableHighlight onPress={()=>this.toggleStatus()}>
-                            <Text>More Details</Text>
+                        <TouchableHighlight onPress={()=> this.toggleStatus()} style={catStyle.card}>
+                            <Text style={catStyle.cardText}>More Details</Text>
                         </TouchableHighlight>
-                        {this.state.status ? <Text>Some Details</Text>: null}
+                        {this.state.status ? 
+                        <ScrollView>
+                            {this.getInfo()}
+                        </ScrollView>
+                        : null}
                     </View>
                 </ScrollView>
                     <Footer />
