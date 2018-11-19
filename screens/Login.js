dbddea2192f799fbff0dc27fd6beb197e6d0514a
leyth adan <mawehelie@gmail.com>
2018-11-18 23:07:30 -0800

    mapDispatchToProps dispatching undefined-empty user object when redirected to profile page

--- screens/Login.js
+++ screens/Login.js
@@ -11,13 +11,17 @@ import Footer from'../components/Footer.js';
 
 import styles from'../styles/Login.js';
 
+import { loginSubmit } from '../actions/authActions';
+import User from 'mspnmodel/distribution/user/user';
+
 
 var width = Dimensions.get('window').width - 18;
 // set up the form model
 const Form = formModel.form.Form;
 
+
 // define the User model
-const User = formModel.struct({
+const TUser = formModel.struct({
   email: formModel.String,
   password: formModel.String,
 });
@@ -40,17 +44,29 @@ const options = {
 class Login extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      email:'',
+      password:''
+    }
+
+    this.onSubmit = this.onSubmit.bind(this);
   }
+
 
   onSubmit() {
     const user = new User(); // might not work! 
     user.copy(this.props.userInfo);
-    this.props.onSubmit(user, this.props.history);
+    this.props.login(user, this.props.history);
+    // EMPTY object 
+  //console.log(user)
   }
 
   // handler used to be here
     render() {
-      
+      // default value for TESTING ONLY
+      this.createValue = {'email': 'Leydth@gmail.com', 'password': 'Password'}
+
         return(
           <View style = {{flex:1}}>
             <ScrollView >
@@ -63,13 +79,14 @@ class Login extends Component {
                   <Text style = {styles.buttonText}>Login with Google</Text>
                 </TouchableOpacity>
                 <Form 
-                  type={User}
+                  type={TUser}
                   ref={c => this._form = c}
                   options={options} 
+                  value={this.createValue}
                 />
                 <Button 
                   title="Login"
-                  onPress={this.handler}
+                  onPress={this.onSubmit}
                   />
               </View>
             </ScrollView>
@@ -78,6 +95,7 @@ class Login extends Component {
         )
     }
 };
+
 
 
     // this links Searcher props to redux store
@@ -90,10 +108,12 @@ class Login extends Component {
       // this links Searcher functions to the dispatcher so we can call sagas.
   const mapDispatchToProps = dispatch => (
       {
-          onSubmit: (userInfo, history) => {
+          login: (userInfo, history) => {
               dispatch(loginSubmit(userInfo));    // call to the saga via action
               history.push("/profile");           // push to new component on completion
-          },
+              console.log(userInfo)
+          
+            },
       }
   );
 // export this 
