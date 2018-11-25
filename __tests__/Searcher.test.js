c5da44047038d2996896eda59a45fee0108c8893
Leyth <mawehelie@gmail.com>
2018-11-25 02:19:44 -0800

    removed enzyme package

--- __tests__/Searcher.test.js
+++ __tests__/Searcher.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Searcher from '../components/Searcher.js'
 import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
 
 describe('Searcher Component Renders Properly', () => {
     xtest('Since Searcher has already been tested', () => {
