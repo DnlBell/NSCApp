2ac756bbde7b5187b9dc137134ca73b7901922ef
Leyth <mawehelie@gmail.com>
2018-11-25 02:17:51 -0800

    skip the searcher test

---
+++ __tests__/Searcher.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Searcher from '../components/Searcher.js'
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+
+describe('Searcher Component Renders Properly', () => {
+    xtest('Since Searcher has already been tested', () => {
+     expect(true).toBe(true);
+    });
+    test('run test to skip', () => {
+     expect(true).toBe(true);
+    });
+  });
