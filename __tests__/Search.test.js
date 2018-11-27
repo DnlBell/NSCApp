2b63728daa1b5a89d67f480459d331aa82b03836
Leyth <mawehelie@gmail.com>
2018-11-26 22:01:01 -0800

    testing saga actions

--- __tests__/Search.test.js
+++ __tests__/Search.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Search from '../screens/Search';
+
 it('renders correctly', () => {
     const tree = renderer
       .create(<Search />)
--- __tests__/cartActions.test.js
+++ __tests__/cartActions.test.js
@@ -3,11 +3,27 @@ import renderer from 'react-test-renderer';
 
 const updateCart = require('../actions/cartActions');
 
-describe('skip test', () => {
-    xtest('tests to be skipped', () => {
-     expect(true).toBe(true);
+// describe('skip test', () => {
+//     xtest('tests to be skipped', () => {
+//      expect(true).toBe(true);
+//     });
+//     test('run test to skip', () => {
+//      expect(true).toBe(true);
+//     });
+//   });
+
+
+  
+  test('updateCart function exists', () => {
+    // mock lodash random to return the value 2 in second test
+    updateCart.updateCart = jest.fn(() => true);
+    expect(updateCart.updateCart()).toBeDefined();
+  });
+
+  test('updateCart function returns an object', () => {
+    // mock lodash random to return the value 2 in second test
+    updateCart.updateCart = jest.fn(() => {
+      return {}
     });
-    test('run test to skip', () => {
-     expect(true).toBe(true);
-    });
+    expect(updateCart.updateCart()).toBeTruthy();
   });
