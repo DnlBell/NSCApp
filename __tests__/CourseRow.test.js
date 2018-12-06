1e9cbc5fc7286a541c32fd672a6fdf2e9c2b927f
Leyth <mawehelie@gmail.com>
2018-12-06 05:23:15 -0800

    test CourseRow

---
+++ __tests__/CourseRow.test.js
@@ -0,0 +1,27 @@
+import CourseRow from '../components/Course/CourseRow';
+import React from 'react';
+import { Text } from 'react-native';
+
+import renderer from 'react-test-renderer';
+import { withRouter } from 'react-router-native';
+import { connect } from 'react-redux';
+import Enzyme, { shallow,mount, configure } from "enzyme";
+import Adapter from 'enzyme-adapter-react-16';
+
+
+const findItem = () => true;
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(withRouter(<CourseRow />))
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(withRouter(<CourseRow />))
+    .toJSON();
+  expect(findItem(tree, 'search')).toBeDefined();
+});
+
