0f103bf3c71f305c3a451f8f50a793fb86b9b3bf
Leyth <mawehelie@gmail.com>
2018-12-06 01:34:04 -0800

    authactions tests passing

--- __tests__/AuthActions.test.js
+++ __tests__/AuthActions.test.js
@@ -1,6 +1,6 @@
 import renderer from 'react-test-renderer';
 import types from '../constants/actionsTypes';
-const actions = require('../actions/cartActions');
+const actions = require('../actions/authActions');
 
   
   test('updateCart function exists', () => {
@@ -19,3 +19,14 @@ const actions = require('../actions/cartActions');
     });
     expect(actions.loginSubmit()).toBeTruthy();
   });
+
+describe('actions', () => {
+  it('should create an action to add a todo', () => {
+    const userinfo = {email: 'Leyth@gmail.com', password: 'Password'};
+    const expectedAction = {
+      type: types.LOGIN_UPDATE_USER_INFO,
+      userinfo
+    }
+    expect(actions.loginSubmit(userinfo)).toEqual(expectedAction)
+  })
+})
