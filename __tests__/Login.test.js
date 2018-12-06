82f9cc800e65a5476bf66acb79292444a562cc08
Leyth <mawehelie@gmail.com>
2018-12-05 22:46:46 -0800

    testing Login file

---
+++ __tests__/Login.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Login from '../screens/Login';
+import { connect } from 'react-redux';
+const findItem = () => true;
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(connect(<Login />))
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+// it('renders correctly', () => {
+//   const tree = renderer
+//     .create(connect(<Login />))
+//     .toJSON();
+//   expect(findItem(tree, 'search')).toBeDefined();
+// });
