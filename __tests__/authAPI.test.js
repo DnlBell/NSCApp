8ae6326c218d21e7586eb549f51f8d1d81207ecb
Leyth <mawehelie@gmail.com>
2018-12-06 04:50:02 -0800

    more ...

---
+++ __tests__/authAPI.test.js
@@ -0,0 +1,17 @@
+import Api from '../api/authApi';
+// import urls from '.../constants/urls';
+import renderer from 'react-test-renderer';
+import React from 'react';
+import fetch from 'isomorphic-fetch';
+// urls.js
+// Definitions of urls
+
+describe('skip test', () => {
+    xtest('tests to be skipped', () => {
+     expect(true).toBe(true);
+    });
+    test('run test to skip', () => {
+     expect(true).toBe(true);
+    });
+  });
+
